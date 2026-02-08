119fc567acbdb8efde051b464cec4f99e4d751d3
Krish3017 <krishramanandi2007@gmail.com>
2026-02-08 12:10:09 +0530

    Last  commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
- base:"/To-Do-List",
+ base:"/CICD",
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
